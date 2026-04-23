b4241712e29d63a1bcf2ad26ae91a8c8153d75ee
Fatou Camille SIMANGOYE <fatou.simangoyecisse@viacesi.fr>
2026-04-24 00:11:42 +0200

    Add files via upload

---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace BillNest.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string? Email { get; set; }     // Le ? permet d'accepter une valeur vide
+        public string? Telephone { get; set; }
+    }
+}
