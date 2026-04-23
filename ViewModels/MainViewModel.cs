1670ae5a94d8b8a84858d6b09bbfe467f8345d01
Fatou Camille SIMANGOYE <fatou.simangoyecisse@viacesi.fr>
2026-04-24 00:21:27 +0200

    Add files via upload

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BillNest.Models;
+
+namespace BillNest.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Client> _clients;
+
+        public ObservableCollection<Client> Clients
+        {
+            get => _clients;
+            set
+            {
+                _clients = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Clients = new ObservableCollection<Client>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
